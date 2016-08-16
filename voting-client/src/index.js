34986732672af7b9e9fe49e125b1ff7ea9938d11
Brian Maddy <brian@brianmaddy.com>
2016-08-16 12:55:53 -0500

    added routing and results screen

--- voting-client/src/index.js
+++ voting-client/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, hashHistory} from 'react-router';
 import './index.css';
+import App from './components/App'
 import Voting from './components/Voting';
+import Results from './components/Results';
 
-const pair = ['Trainspotting', '28 Days Later'];
+const routes = <Route component={App}>
+  <Route path="/" component={Voting} />
+  <Route path="/results" component={Results} />
+</Route>;
 
 ReactDOM.render(
-  <Voting pair={pair} winner="Trainspotting"/>,
+  <Router history={hashHistory}>{routes}</Router>,
   document.getElementById('root')
 );
